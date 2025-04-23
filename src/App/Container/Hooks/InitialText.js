50a88b48e81265ab564b80d43d9bd35ab57142d9
marcop135 <marcop135.github@gmail.com>
2025-04-23 12:32:01 +0200

    fix: remove typo in md preview

--- src/App/Container/Hooks/InitialText.js
+++ src/App/Container/Hooks/InitialText.js
@@ -1,4 +1,4 @@
-export const initialText = `## Hello World!
+export const initialText = `# Hello World!
 
 **Markdown2DPF** is a neat, tidy, offline [Markdown](https://daringfireball.net/projects/markdown/) to PDF conversion app that works directly in your browser.
 `;
